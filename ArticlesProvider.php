b624fd47b32ff54241fd6a3cc6589ecfcdd239bb
Gennadiy <gennadiy_bilyk@mail.ru>
2017-04-16 11:17:32 +0300

    first commit

---
+++ ArticlesProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GennadiyBilyk\Articles;
+
+use Illuminate\Support\ServiceProvider;
+
+class ArticlesProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
